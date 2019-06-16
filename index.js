9be741972edbd9a8ca550b36d4c759fd16e76512
LoFeBT <51871740+LoFeBT@users.noreply.github.com>
2019-06-16 00:07:16 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var Discord = require('discord.js');
+var bot = new Discord.Client();
+var isReady = true;
+
+bot.on('message', message => {
+  if (isReady && message.content === 'OOF')
+  {
+  isReady = false;
+  var voiceChannel = message.member.voiceChannel;
+  voiceChannel.join().then(connection =>
+  {
+     const dispatcher = connection.playFile('./Audio/OOF.mp3');
+     dispatcher.on("end", end => {
+       voiceChannel.leave();
+       });
+   }).catch(err => console.log(err));
+   isReady = true;
+  }
+});
+
+
+bot.login(process.env.BOT_TOKEN);
